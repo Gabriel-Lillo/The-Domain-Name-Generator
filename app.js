0088a3244b796114c4027c6fe5fd8d88805148e0
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2021-04-28 17:54:26 -0400

    terminado

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-let pronoun = ['the','our'];
-let adj = ['great','big'];
-let noun = ['jogger','racoon'];
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.us', '.io']
+
+
+let pro = Math.floor(Math.random() * pronoun.length);
+let ad = Math.floor(Math.random() * adj.length);
+let nou = Math.floor(Math.random() * noun.length);
+let exten = Math.floor(Math.random() * extension.length);
+
+
+console.log(pronoun[pro] + adj[ad] + noun[nou] + extension[exten]);
+
+
+
+
+
